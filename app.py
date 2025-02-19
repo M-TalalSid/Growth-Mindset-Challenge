5bfd9e014e8babc2f31085327b2db552818a3095
Talal Shoaib <160588772+M-TalalSid@users.noreply.github.com>
2025-02-19 05:20:09 +0500

    Update app.py

--- app.py
+++ app.py
@@ -168,4 +168,4 @@ if st.button("Export Progress as CSV"):
 
 # Footer
 st.markdown("---")
-st.markdown("Copyright © 2025 My Streamlit App. All Rights Reserved.")
+st.markdown("©2025 Streamlit App. All Rights Reserved. Made By Talal Shoaib")
